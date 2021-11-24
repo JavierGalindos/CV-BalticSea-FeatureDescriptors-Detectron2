8acb193f99f586abd6a7f1b1e65e3634978cf50b
Avic3nna <omarelnahhas1337@gmail.com>
2021-11-24 20:32:10 +0200

    Automated pipeline for feature detector

---
+++ fd_detector.py
@@ -0,0 +1,307 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import os
+import re
+from feat_matchers import orb_detector, brief_detector, sift_detector, fill_bounding_boxes
+
+from skimage import data, filters, color, morphology
+from skimage.segmentation import flood, flood_fill
+from skimage.measure import label, regionprops
+from bs4 import BeautifulSoup
+from mean_average_precision import MetricBuilder
+
+# Read the query image as query_img
+# and train image This query image
+# is what you need to find in train image
+# Save it in the same directory
+# with the name image.jpg 
+
+
+# PICK AN IMAGE 
+
+def fd_detector(method = 'SIFT', lowe_ratio = 0.6, good_match_thresh = 0):
+    process_img_path = './Processed images/'
+    
+    #input argument to choose detector
+    for type_of_class in os.listdir('./Processed images/'):
+        process_img_path = os.path.join(process_img_path, type_of_class)
+        for img_path in os.listdir(process_img_path):
+            full_img_path = os.path.join(process_img_path, img_path)
+            print('Now processing: %s' % img_path)
+            train_img_bw = cv2.imread(full_img_path,0)
+
+            grid_per_side = 4
+            step = int(train_img_bw.shape[0]/grid_per_side) # 4x4 matrix
+
+            grid_class = np.empty(shape = (grid_per_side, grid_per_side), dtype='object')
+
+            classes = ['Fuc', 'Fur', 'Zos']
+
+            for x_coord in range(0, train_img_bw.shape[0], step):
+                begin_x = x_coord
+                end_x = x_coord + step
+                for y_coord in range(0, train_img_bw.shape[1], step):
+                    begin_y = y_coord
+                    end_y = y_coord + step
+                    
+                    #step through every grid cell
+                    img_patch = train_img_bw[begin_x:end_x, begin_y:end_y]
+                    
+                    class_for_patch = []
+                    #now check this image patch with all the etalons, return the max class (or nothing) per img patch
+                    for folder in os.listdir('./Etalons'):
+                        #get to a particular class of etalon
+                        etalon_dir_path = os.path.join('./Etalons', folder)
+                        
+                        max_goodies = 0
+                        
+                        for etalon_img_path in os.listdir(etalon_dir_path):
+                            
+                            etalon_fullpath = os.path.join(etalon_dir_path, etalon_img_path)
+                            
+                            # Now detect the keypoints and compute
+                            # the descriptors for the query image
+                            # and train image
+                            query_img_bw = cv2.imread(etalon_fullpath,0)
+                            #print(etalon_fullpath)
+                            if method == 'ORB':
+                                good_matches = orb_detector(query_img_bw, img_patch, lowe_ratio)
+                            elif method == 'BRIEF':
+                                good_matches = brief_detector(query_img_bw, img_patch, lowe_ratio)
+                            else: #SIFT, best one
+                                good_matches = sift_detector(query_img_bw, img_patch, lowe_ratio)
+                            
+                            # img3 = cv2.drawMatchesKnn(query_img_bw,queryKeypoints,img_patch,trainKeypoints,good, None, flags=2)
+                            max_goodies += good_matches
+
+                        class_for_patch.append(max_goodies)
+                    #print((class_for_patch))
+                    if np.max(class_for_patch) > good_match_thresh:
+                        #deal with multiple maxima at one point
+                        m = [i for i,j in enumerate(class_for_patch) if j==np.max(class_for_patch)]
+                    
+                        pred_patch_class = ''
+                        for i in m:
+                            pred_patch_class += classes[i]
+
+                    else:
+                        pred_patch_class = 'None'
+                        
+                    #print(pred_patch_class)
+                    grid_class[int(begin_x/step),int(begin_y/step)] = pred_patch_class
+            
+            
+            ## second cell
+            
+            len_per_class = 3
+            grid_vect = np.zeros(16, dtype='object')
+            nr = 0
+            numerical_grid = np.zeros((grid_per_side, grid_per_side), dtype='float')
+            for i in range(grid_per_side):
+                for j in range(grid_per_side):
+                    #numerical_grid[i][j] = round(len_per_class/len(grid_class[i][j]),2)
+                    numerical_grid[i][j] = nr
+                    grid_vect[nr] = grid_class[i][j]
+                    nr +=1
+            
+            ## third cell
+            classes = ['Fuc', 'Fur', 'Zos', 'None']
+
+            box10 = [0,1,4,5]
+            box11 = [1,2,5,6]
+            box12 = [2,3,6,7]
+
+            box20 = [4,5,8,9]
+            box21 = [5,6,9,10]
+            box22 = [6,7,10,11]
+
+            box30 = [8,9,12,13]
+            box31 = [9,10,13,14]
+            box32 = [10,11,14,15]
+
+            all_windows = [box10, box11, box12, box20, box21, box22, box30, box31,box32]
+            windows_names = ['box10', 'box11', 'box12', 'box20', 'box21', 'box22', 'box30', 'box31','box32']
+            i = 0
+            best_class = np.zeros(9, dtype='object')
+            best_class_num = np.zeros(9, dtype='int')
+            best_class_conf = np.zeros(9, dtype='float')
+            
+            #print(grid_vect)
+            
+            for box in all_windows:
+
+                fuc = 0
+                fur = 0
+                zos = 0
+                no = 0
+                #print(grid_vect[box])
+                for element in grid_vect[box]:
+                    #print(element)
+                    #if the are multiple elements, we give it a lower score per class
+                    #because we don't award confusion :)
+                    if(len(element) == 9):
+                        multiplier = (1/3)
+                    elif(len(element) == 6):
+                        multiplier = (1/2)
+                    else:
+                        multiplier = 1
+                    fuc += multiplier*len(re.findall('Fuc', element))
+                    fur += multiplier*len(re.findall('Fur', element))
+                    zos += multiplier*len(re.findall('Zos', element))
+                    no += multiplier*len(re.findall('None', element))
+                
+
+                # print(fuc)
+                # print(fur)
+                # print(zos)
+                # print(no)
+                #find all the occurences of maximum class
+                m = [skd for skd,j in enumerate((fuc, fur, zos, no)) if j==np.max((fuc, fur, zos, no))]
+                #more than 2 with same likelihood? then just None to avoid confusion
+                #print(m)
+                if(len(m) > 1):
+                    best_class[i] = classes[3] #none
+                    best_class_num[i] = 3
+                else:
+                    best_class[i] = classes[np.argmax((fuc, fur, zos, no))]
+                    best_class_num[i] = np.argmax((fuc, fur, zos, no))
+                
+                best_class_conf[i] = np.round((np.max((fuc, fur, zos, no))/4), 4)
+                
+                #print('%s: %s' %(windows_names[i], best_class[i]))
+                i+=1
+            
+            ## fourth cell
+            best_class = best_class.reshape((3,3))
+            best_class_num = best_class_num.reshape((3,3))
+            best_class_conf = best_class_conf.reshape((3,3))
+            
+            #fifth cell
+            
+            all_indeces = [(0,0), (0,1), (0,2), (1, 0), (1, 1), (1, 2), (2, 0), (2,1), (2, 2)]
+            coordinates = np.zeros((3,3), dtype = 'int')
+
+            for index in all_indeces:
+                #get the value of the index
+                x = index[0]
+                y = index[1]
+                if best_class_num[x][y] >= 0:
+                    #map to specific class, -1 is fucus, -2 is furcellaria, -3 is Zos and otherwise its None
+                    value = (-1 if best_class_num[x][y] == 0 else -2 if best_class_num[x][y] == 1 else -3 if best_class_num[x][y] == 2 else -4)
+                    #filled_checkers = flood_fill(best_class_num, index, value)
+                    findconn = flood(best_class_num, index)
+                
+                    
+                    if(np.sum(findconn)) > 1:
+                        where_is_it = np.where(findconn == True)
+                        for i in range(len(where_is_it[0])):
+                            o = where_is_it[0][i]
+                            u = where_is_it[1][i]
+                            coordinates[o][u] = value
+                        
+            seperation = coordinates.copy()
+            classes, coords = fill_bounding_boxes(coordinates)
+            
+            
+            conf = []
+
+            for i in range(len(classes)):
+                slice = coords[i]
+                this_class = classes[i]
+                find_rectangle = (seperation[slice] == this_class)
+                cropped_conf = best_class_conf[slice]
+                
+                #print(this_class)
+                #print(seperation[slice])
+                    
+                conf.append(sum(cropped_conf[find_rectangle])/(np.prod(find_rectangle.shape))) # avg confidence
+            
+            step = int(256*(4/3))
+            pred = []
+
+            for x in range(len(conf)):
+                this_slice = coords[x]
+                x_begin = this_slice[0].start
+                x_end = this_slice[0].stop
+                y_begin = this_slice[1].start
+                y_end = this_slice[1].stop
+                
+                #print(this_slice[0])
+                
+                this_conf = conf[x]
+                this_class = classes[x]
+                
+                class_name = ['Fucus', 'Furcellaria', 'Zostera', 'None']
+                #print(this_class)
+                class_single = class_name[(-this_class-1)] #name string
+                
+                pred.append((x_begin*step, y_begin*step, x_end*step, y_end*step, int(-this_class-1), this_conf))
+                
+                # Plot image
+                
+                color_list = [[0,0,255], [0,255,0], [255,0,0], [0,0,0]]
+                color = color_list[(-value-1)]
+                thickness = 2
+
+                outputstr = class_single + ': ' + str(this_conf)
+                        
+                image = cv2.rectangle(cv2.cvtColor(train_img_bw, cv2.COLOR_GRAY2RGB), (int(x_begin*step), int(y_begin*step)), (int(x_end*step + step), int(y_end*step+step)), color, thickness)     
+                image = cv2.putText(image, outputstr, ((int(x_begin*step)+5), int(y_begin*step)+30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+
+            pred = np.array(pred) 
+            #imS = cv2.resize(image, (960, 540))       
+            cv2.imshow('FD output',image) #resize to fit img
+            #cv2.moveWindow('FD output', 40,30)  # Move it to (40,30)
+            cv2.waitKey(0)
+            
+            
+                                # Reading the data inside the xml
+            # file to a variable under the name
+            # data
+            with open('./all_classes_cv/annotations.xml', 'r') as f:
+                data = f.read()
+            
+            # Passing the stored data inside
+            # the beautifulsoup parser, storing
+            # the returned object
+            Bs_data = BeautifulSoup(data, "xml")
+            
+            # Using find() to extract attributes
+            # of the first instance of the tag
+            b_name = Bs_data.find('image',{'name': img_path})
+
+            box_data = b_name.find('box')
+            xbr = int(float(box_data.get('xbr')))
+            xtl = int(float(box_data.get('xtl')))
+            ybr = int(float(box_data.get('ybr')))
+            ytl = int(float(box_data.get('ytl')))
+            label = str(box_data.get('label'))
+
+            #print(label)
+            class_name = ['Fucus', 'Furcellaria', 'Zostera', 'None']
+            label_num = class_name.index(label)
+
+            #print(label_num)
+            #https://pypi.org/project/mean-average-precision/
+            gt = np.array([[xtl, ytl, xbr, ybr, label_num, 0, 0]], dtype=np.int64) #idk about the 0,0 with
+            
+            
+            # create metric_fn
+            metric_fn = MetricBuilder.build_evaluation_metric("map_2d", async_mode=True, num_classes=4)
+
+            # add some samples to evaluation
+            for i in range(10):
+                metric_fn.add(pred, gt)
+                
+            # compute PASCAL VOC metric
+            print(f"VOC PASCAL mAP: {metric_fn.value(iou_thresholds=0.5, recall_thresholds=np.arange(0., 1.1, 0.1))['mAP']}")
+
+            # compute PASCAL VOC metric at the all points
+            print(f"VOC PASCAL mAP in all points: {metric_fn.value(iou_thresholds=0.5)['mAP']}")
+
+            # compute metric COCO metric
+            print(f"COCO mAP: {metric_fn.value(iou_thresholds=np.arange(0.5, 1.1, 0.1), recall_thresholds=np.arange(0., 1.01, 0.01), mpolicy='soft')['mAP']}")
+                    
+if __name__ == '__main__':
+    fd_detector()
---
+++ feat_matchers.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+from skimage.measure import label, regionprops
+
+def orb_detector(query_img_bw, img_patch, lowe_ratio):
+    orb = cv2.ORB_create()
+    queryKeypoints, queryDescriptors = orb.detectAndCompute(query_img_bw,None)
+    trainKeypoints, trainDescriptors = orb.detectAndCompute(img_patch,None)
+    
+    # Initialize the Matcher for matching
+    # the keypoints and then match the
+    # keypoints
+
+    #matcher = cv2.DescriptorMatcher_create('BruteForce-L1') #BruteForce = L2, BruteForce-L1
+    matcher = cv2.BFMatcher(cv2.NORMCONV_FILTER)
+    matches = matcher.knnMatch(queryDescriptors,trainDescriptors, k=2)
+    
+    # draw the matches to the final image
+    # containing both the images the drawMatches()
+    # function takes both images and keypoints
+    # and outputs the matched query image with
+    # its train image
+    # final_img = cv2.drawMatchesKnn(query_img_bw, queryKeypoints,
+    # img_patch, trainKeypoints, matches[:50],None)
+    good = [[m] for m, n in matches if m.distance < lowe_ratio*n.distance]
+    
+    return len(good)
+
+
+def brief_detector(query_img_bw, img_patch, lowe_ratio):
+    # BRIEF is only a descriptor not finder
+    # Initiate FAST detector
+    finder = cv2.xfeatures2d.StarDetector_create()
+    # Initiate BRIEF extractor
+    brief = cv2.xfeatures2d.BriefDescriptorExtractor_create()
+    # find the keypoints and descriptors with FAST
+    kp1 = finder.detect(query_img_bw,None)
+    kp2 = finder.detect(img_patch,None)
+    kp1, des1 = brief.compute(query_img_bw, kp1)
+    kp2, des2 = brief.compute(img_patch, kp2)
+
+    # BFMatcher with hamming >> L2
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+    matches = bf.knnMatch(des1,des2, k=2)
+    # Apply ratio test
+    good = [[m] for m, n in matches if m.distance < lowe_ratio*n.distance]
+    
+    return len(good)
+
+
+def sift_detector(query_img_bw, img_patch, lowe_ratio):
+    # Initiate SIFT detector
+    sift = cv2.SIFT_create()
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(query_img_bw,None)
+    kp2, des2 = sift.detectAndCompute(img_patch,None)
+    
+    length = 0
+    
+    # FLANN parameters
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks=50)   # or pass empty dictionary
+    flann = cv2.FlannBasedMatcher(index_params,search_params)
+    matches = flann.knnMatch(des1,des2,k=2)
+    # Need to draw only good matches, so create a mask
+
+    good = []
+    # ratio test as per Lowe's paper
+    for m,n in (matches):
+        if m.distance <lowe_ratio*n.distance:
+            good.append(m)
+            
+    return len(good)
+
+
+def fill_bounding_boxes(z):
+    box_class = []
+    box_coords = []
+    l = label(z)
+    print(l)
+    # print(l)
+    # print(regionprops(l))
+    x_og = z.copy()
+    
+    for s in regionprops(l):
+        values, counts = np.unique(x_og[s.slice], return_counts=True)
+        #print(np.unique(x_og[s.slice]))
+        # print(values)
+        # print(counts)
+        ind = np.argmax(counts)
+        # print(values[ind])  # prints the most frequent element
+        z[s.slice] = values[ind]
+        box_class.append(z[s.slice][0][0]) #homogeneous so just take any value from it
+        box_coords.append(s.slice) #save the coords
+        print(s.slice)
+        print(z[s.slice])
+    return box_class, box_coords
